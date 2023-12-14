74ab0c8c59baf9672b55057f8057c02304e4febc
Abi123-suara <abinash998143@gmail.com>
2023-12-14 12:41:28 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from app.models import *
 # Create your views here.
+ 
+def department(request):
+    QLDO=Dept.objects.all()
+    d={'depts' : QLDO}
+    return render(request,'department.html',d)
+
+def employee(request):
+    QLEO=Emp.objects.all()
+    d={'emp' : QLEO}
+    return render(request,'employee.html',d)
--- project25/urls.py
+++ project25/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('department/',department,name='department'),
+    path('employee/',employee,name='employee')
+    
 ]
